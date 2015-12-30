5a06b1bb7420e4bab182d54d840e02ddb6b7dd91
nikolai <nikolaikonan@github.com>
2015-12-30 11:59:30 +0300

    added js alg Korobko

---
+++ 05 js advanced/homework/js_Korobko.js
@@ -0,0 +1,18 @@
+var x = -40;
+var y = -60; 
+var xp = new Array(-73,-33,7,-33); // Массив X-координат полигона
+var yp = new Array(-85,-126,-85,-45); // Массив Y-координат полигона
+function inPoly(x,y){
+  npol = xp.length;
+  j = npol - 1;
+  var c = 0;
+  for (i = 0; i < npol;i++){
+      if ((((yp[i]<=y) && (y<yp[j])) || ((yp[j]<=y) && (y<yp[i]))) &&
+      (x > (xp[j] - xp[i]) * (y - yp[i]) / (yp[j] - yp[i]) + xp[i])) {
+       c = !c
+       }
+       j = i;
+  }
+return c;
+}
+inPoly(x,y);
